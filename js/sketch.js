a2f7ca0d19c82fc3ebc7b351f7ef09e725200d82
music-matching <daqueenspeakstruth@gmail.com>
2016-12-03 19:06:41 -0500

    Update sketch.js

--- js/sketch.js
+++ js/sketch.js
@@ -7,7 +7,7 @@ var mic, recorder, soundFile;
 var state = 0; // mousePress will increment from Record, to Stop, to Play
 
 function setup() {
-  createCanvas(400,400);
+  createCanvas(100,100);
     background(0,0,0,0);
 //   fill(0);
   text('Enable mic and click the mouse to begin recording', 20, 20);
@@ -37,23 +37,30 @@ function mousePressed() {
     // Tell recorder to record to a p5.SoundFile which we will use for playback
     function recordSound(){
       recorder.record(soundFile);
-    }
-    background(255,0,0);
+    
+   background(255,0,0);
     text('Recording now! Click to stop.', 20, 20);
     state++;
+      }
   }
 
   else if (state === 1) {
+        function recordStop(){
+
     recorder.stop(); // stop recorder, and send the result to soundFile
 
     background(0,255,0);
     text('Recording stopped. Click to play & save', 20, 20);
     state++;
+        }
   }
 
   else if (state === 2) {
     soundFile.play(); // play the result!
+            function recordSave(){
+
     saveSound(soundFile, 'js/mySound.wav'); // save file
     state++;
+            }
   }
 }
